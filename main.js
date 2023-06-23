9b5670fee2f0bb09966434e319096c2b4f81b887
ssantiago90 <salkin.santiago@gmail.com>
2023-06-23 16:07:49 -0300

    initial code

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-// Seleccionar el contenedor al que queremos agregar un elemento
-const container = document.querySelector("body");
-
-// Crear el nuevo elemento HTML <h1>
-const footerNode = document.createElement("footer");
-footerNode.textContent = "Hola Footer";
-footerNode.classList.add("footer");
-
-// Agregar el nodo h1 creado al elemento contenedor
-container.appendChild(footerNode);
+console.log("main.js");
+const navNode = document.querySelector("nav");
+console.log("Nodo:", navNode);
